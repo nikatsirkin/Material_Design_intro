c6e55bfbef6f68bfcfd6401b4397cc1e40072ad5
Nika <tsirkinnika@gmail.com>
2020-07-09 19:10:51 +0200

    total invites

--- app/src/main/java/com/example/tabsexample/HardwareFragment.java
+++ app/src/main/java/com/example/tabsexample/HardwareFragment.java
@@ -2,19 +2,29 @@ package com.example.tabsexample;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 public class HardwareFragment extends Fragment {
 
-
+    private TextView tvToatal;
     public HardwareFragment() {
         // Required empty public constructor
     }
@@ -37,5 +47,30 @@ public class HardwareFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hardware, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        tvToatal =  getView().findViewById(R.id.tv_total);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference busInvite = db.collection("BusInvite");
+        busInvite.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                            }
+                            tvToatal.setText("Total invites: " + task.getResult().size());
+                        } else {
+                            Log.d("TAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        //
     }
 }
